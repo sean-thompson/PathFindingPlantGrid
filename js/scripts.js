babe0262df52e41cdf9685749b3a42a7b16678e1
= <sean.thompson@dubitlimited.com>
2023-06-16 16:30:33 +0100

    more performant now (though there's still a lot of html manipulation that should be replaced)

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,40 @@
-  // Define the number of rows and cells
-  const numCells = 14; // Number of cells per row
-  const numRows = 13; // Number of rows
-  const sampleSize = 3; // Size of the sample grid
-  let sampleArray = []; // Array to store the changed samples
+// Define the number of rows and cells
+const numCells = 14; // Number of cells per row
+const numRows = 13; // Number of rows
+const sampleSize = 3; // Size of the sample grid
+let sampleArray = []; // Array to store the changed samples
 
-  let element;//closest element to mouse
-  let offsetX;//closest parent td to mouse index
-  let offsetY;//closest parent tr to mouse index
+let element;//closest element to mouse
+let offsetX;//closest parent td to mouse index
+let offsetY;//closest parent tr to mouse index
 
-  let table;//the garden
+let table;//the garden
+let tableContents = [];
 
-  // Predefined list of images to switch between
-  const imageList = [
-    "./img/dirt.png",
-    "./img/bluebells.png",
-    "./img/bracken.png",
-    "./img/brambles.png",
-    "./img/cottongrass.png",
-    "./img/moss.png",
-    "./img/mushrooms.png",
-    "./img/wildgarlic.png"
-  ];
+// Predefined list of images to switch between
+const imageList = [
+  "./img/dirt.png",
+  "./img/bluebells.png",
+  "./img/bracken.png",
+  "./img/brambles.png",
+  "./img/cottongrass.png",
+  "./img/moss.png",
+  "./img/mushrooms.png",
+  "./img/wildgarlic.png"
+];
 
-  //trees
-  const trees = {
-    "oak":
-      {"data":[[0,7,1,7,0],[7,1,7,7,1],[7,7,5,7,7],[7,1,7,1,7],[0,7,1,7,0]],
-        "ox":26, "oy":55, "location":"./img/oaktree.png"},
-    "birch":
-      {"data":[[0,2,2,2,0],[2,6,2,2,6],[2,2,6,2,2],[2,2,2,2,2],[0,2,2,6,0]],
-      "ox":20, "oy":45, "location":"./img/silverbirch.png"},
-    "pine":
-      {"data":[[0,3,4,4,0],[4,4,4,3,4],[3,4,3,4,3],[4,4,3,4,4],[0,4,4,3,0]],
-      "ox":25, "oy":55, "location":"./img/pinetree.png"}
-  }
+//trees
+const trees = {
+  "oak":
+    {"data":[[0,7,1,7,0],[7,1,7,7,1],[7,7,5,7,7],[7,1,7,1,7],[0,7,1,7,0]],
+      "ox":26, "oy":55, "location":"./img/oaktree.png"},
+  "birch":
+    {"data":[[0,2,2,2,0],[2,6,2,2,6],[2,2,6,2,2],[2,2,2,2,2],[0,2,2,6,0]],
+    "ox":20, "oy":45, "location":"./img/silverbirch.png"},
+  "pine":
+    {"data":[[0,3,4,4,0],[4,4,4,3,4],[3,4,3,4,3],[4,4,3,4,4],[0,4,4,3,0]],
+    "ox":25, "oy":55, "location":"./img/pinetree.png"}
+}
 
 function createTable(){
   // Create the table element
@@ -41,10 +42,12 @@ function createTable(){
   
   // Create the table rows with data
   for (var i = 0; i < numRows; i++) {
+    tableContents.push([]);
     var row = $("<tr>");
 
     // Add cells with images
     for (var j = 0; j < numCells; j++) {
+      tableContents[i].push(0);
       var cell = $("<td>");
       var imageIndex = 0; // First image in imageList is soil
       var imageSrc = imageList[imageIndex];
@@ -78,82 +81,30 @@ function createTable(){
 
   // Append the table to the body
   $("body").append(table);
-
   return table;
 }
 
 function updateSoil() {
-  // Find all <td> elements within the table
-  var $tdElements = table.find('td');
-
-  // Iterate through each <td> element
-  $tdElements.each(function() {
-    // Check if the <td> contains an image
-    var $imgElements = $(this).find('img');
-    if ($imgElements.length > 0) {
-      var hasDirtImage = $imgElements.filter(function() {
-        return $(this).attr('src') === imageList[0];
-      }).length > 0;
-
-      // Change the background color of the <td> to yellow
-      if (!hasDirtImage) {
-        $(this).css('background-color', 'sandybrown');
+  for (var y = 0; y < tableContents.length; y++) {
+    for (var x = 0; x < tableContents[y].length; x++) {
+      if (tableContents[y][x] !== 0) {
+        table.find('tr').eq(y).find('td').eq(x).css('background-color', 'yellow');
+      } else if (
+        (y > 0 && tableContents[y - 1][x] !== 0) || // Above
+        (y > 0 && x > 0 && tableContents[y - 1][x - 1] !== 0) || // Above Left
+        (x > 0 && tableContents[y][x - 1] !== 0) || // Left
+        (y < tableContents.length - 1 && x > 0 && tableContents[y + 1][x - 1] !== 0) || // Below Left
+        (y < tableContents.length - 1 && tableContents[y + 1][x] !== 0) || // Below
+        (y < tableContents.length - 1 && x < tableContents[y].length - 1 && tableContents[y + 1][x + 1] !== 0) || // Below Right
+        (x < tableContents[y].length - 1 && tableContents[y][x + 1] !== 0) || // Right
+        (y > 0 && x < tableContents[y].length - 1 && tableContents[y - 1][x + 1] !== 0) // Above Right
+      ) {
+        table.find('tr').eq(y).find('td').eq(x).css('background-color', 'cyan');
       } else {
-         // Find all <td> elements within the table
-        var $tdElements = table.find('td');
-
-        // Iterate through each <td> element
-        $tdElements.each(function() {
-          // Check if the <td> contains an image
-          var $imgElements = $(this).find('img');
-          if ($imgElements.length > 0 && $imgElements.attr('src') === './img/dirt.png') {
-            var rowIndex = this.parentNode.rowIndex;
-            var cellIndex = this.cellIndex;
-
-            var hasAdjacentDirtImage = false;
-
-            // Check if any adjacent <td> elements contain the dirt image
-            // Above
-            if (rowIndex > 0) {
-              var $aboveTD = table.find('tr').eq(rowIndex - 1).find('td').eq(cellIndex);
-              if ($aboveTD.find('img').length === 0 || $aboveTD.find('img').attr('src') !== './img/dirt.png') {
-                hasAdjacentDirtImage = true;
-              }
-            }
-
-            // Below
-            if (rowIndex < table.find('tr').length - 1) {
-              var $belowTD = table.find('tr').eq(rowIndex + 1).find('td').eq(cellIndex);
-              if ($belowTD.find('img').length === 0 || $belowTD.find('img').attr('src') !== './img/dirt.png') {
-                hasAdjacentDirtImage = true;
-              }
-            }
-
-            // Left
-            if (cellIndex > 0) {
-              var $leftTD = $(this).prev('td');
-              if ($leftTD.find('img').length === 0 || $leftTD.find('img').attr('src') !== './img/dirt.png') {
-                hasAdjacentDirtImage = true;
-              }
-            }
-
-            // Right
-            if (cellIndex < $(this).siblings('td').length) {
-              var $rightTD = $(this).next('td');
-              if ($rightTD.find('img').length === 0 || $rightTD.find('img').attr('src') !== './img/dirt.png') {
-                hasAdjacentDirtImage = true;
-              }
-            }
-
-            // Change the background color of the <td> to saddlebrown if no adjacent <td> contains the dirt image
-            if (hasAdjacentDirtImage) {
-              $(this).css('background-color', 'saddlebrown');
-            }
-          }
-        });
+        table.find('tr').eq(y).find('td').eq(x).css('background-color', 'magenta');
       }
     }
-  });
+  }
 }
 
 $(document).ready(function() {
@@ -203,6 +154,8 @@ $(document).ready(function() {
     } else if(event.key >= 1 && event.key <= 7) {
       var cellImage = table.find("tr").eq(offsetY).find("td").eq(offsetX).find("img");
 
+      tableContents[offsetY][offsetX] = event.key;
+
       if(offsetY >= 0 && offsetX >= 0){
         cellImage.hide();
         cellImage.attr("src", imageList[event.key]);
@@ -232,6 +185,9 @@ $(document).ready(function() {
           var cellImage = table.find("tr").eq(y+offsetY-2).find("td").eq(x+offsetX-2).find("img");
 
             if(cellImage.attr("src") == imageList[0] && y+offsetY-2 >= 0 && x+offsetX-2 >= 0){
+              
+              tableContents[y+offsetY-2][x+offsetX-2] = data[x][y];
+
               cellImage.hide();
               cellImage.attr("src", imageList[data[x][y]]);
               cellImage.fadeIn();
