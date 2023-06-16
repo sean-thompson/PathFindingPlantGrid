e97eba591fee483fda8445b4ba5f65d6afaa4d28
= <sean.thompson@dubitlimited.com>
2023-06-16 11:53:41 +0100

    sets soild types (but it's not very performant)

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
   // Define the number of rows and cells
-  const numCells = 40; // Number of cells per row
-  const numRows = 30; // Number of rows
+  const numCells = 14; // Number of cells per row
+  const numRows = 13; // Number of rows
   const sampleSize = 3; // Size of the sample grid
   let sampleArray = []; // Array to store the changed samples
 
   let element;//closest element to mouse
   let offsetX;//closest parent td to mouse index
   let offsetY;//closest parent tr to mouse index
+
+  let table;//the garden
 
   // Predefined list of images to switch between
   const imageList = [
@@ -35,7 +37,7 @@
 
 function createTable(){
   // Create the table element
-  var table = $("<table>").addClass("my-table");
+  table = $("<table>").addClass("my-table");
   
   // Create the table rows with data
   for (var i = 0; i < numRows; i++) {
@@ -80,6 +82,80 @@ function createTable(){
   return table;
 }
 
+function updateSoil() {
+  // Find all <td> elements within the table
+  var $tdElements = table.find('td');
+
+  // Iterate through each <td> element
+  $tdElements.each(function() {
+    // Check if the <td> contains an image
+    var $imgElements = $(this).find('img');
+    if ($imgElements.length > 0) {
+      var hasDirtImage = $imgElements.filter(function() {
+        return $(this).attr('src') === imageList[0];
+      }).length > 0;
+
+      // Change the background color of the <td> to yellow
+      if (!hasDirtImage) {
+        $(this).css('background-color', 'sandybrown');
+      } else {
+         // Find all <td> elements within the table
+        var $tdElements = table.find('td');
+
+        // Iterate through each <td> element
+        $tdElements.each(function() {
+          // Check if the <td> contains an image
+          var $imgElements = $(this).find('img');
+          if ($imgElements.length > 0 && $imgElements.attr('src') === './img/dirt.png') {
+            var rowIndex = this.parentNode.rowIndex;
+            var cellIndex = this.cellIndex;
+
+            var hasAdjacentDirtImage = false;
+
+            // Check if any adjacent <td> elements contain the dirt image
+            // Above
+            if (rowIndex > 0) {
+              var $aboveTD = table.find('tr').eq(rowIndex - 1).find('td').eq(cellIndex);
+              if ($aboveTD.find('img').length === 0 || $aboveTD.find('img').attr('src') !== './img/dirt.png') {
+                hasAdjacentDirtImage = true;
+              }
+            }
+
+            // Below
+            if (rowIndex < table.find('tr').length - 1) {
+              var $belowTD = table.find('tr').eq(rowIndex + 1).find('td').eq(cellIndex);
+              if ($belowTD.find('img').length === 0 || $belowTD.find('img').attr('src') !== './img/dirt.png') {
+                hasAdjacentDirtImage = true;
+              }
+            }
+
+            // Left
+            if (cellIndex > 0) {
+              var $leftTD = $(this).prev('td');
+              if ($leftTD.find('img').length === 0 || $leftTD.find('img').attr('src') !== './img/dirt.png') {
+                hasAdjacentDirtImage = true;
+              }
+            }
+
+            // Right
+            if (cellIndex < $(this).siblings('td').length) {
+              var $rightTD = $(this).next('td');
+              if ($rightTD.find('img').length === 0 || $rightTD.find('img').attr('src') !== './img/dirt.png') {
+                hasAdjacentDirtImage = true;
+              }
+            }
+
+            // Change the background color of the <td> to saddlebrown if no adjacent <td> contains the dirt image
+            if (hasAdjacentDirtImage) {
+              $(this).css('background-color', 'saddlebrown');
+            }
+          }
+        });
+      }
+    }
+  });
+}
+
 $(document).ready(function() {
 
   const table = createTable();
@@ -96,9 +172,35 @@ $(document).ready(function() {
     offsetY = element.closest("tr").index();
   });
 
-  //plants
+  //trees
   $(document).keydown(function(event) {
-    if (event.key >= 1 && event.key <= 7) {
+    let image, dx, dy, scale, data;
+
+    if (event.key === "q") {
+      image = $('<img>', {
+        src: trees.oak.location
+      });
+      dx = -trees.oak.ox;
+      dy = -trees.oak.oy;
+      scale = trees.oak.scale;
+      data = trees.oak.data;
+    } else if (event.key === "w") {
+      image = $('<img>', {
+        src: trees.birch.location
+      });
+      dx = -trees.birch.ox;
+      dy = -trees.birch.oy;
+      scale = trees.birch.scale;
+      data = trees.birch.data;
+    } else if (event.key === "e") {
+      image = $('<img>', {
+        src: trees.pine.location
+      });
+      dx = -trees.pine.ox;
+      dy = -trees.pine.oy;
+      scale = trees.pine.scale;
+      data = trees.pine.data;
+    } else if(event.key >= 1 && event.key <= 7) {
       var cellImage = table.find("tr").eq(offsetY).find("td").eq(offsetX).find("img");
 
       if(offsetY >= 0 && offsetX >= 0){
@@ -106,42 +208,12 @@ $(document).ready(function() {
         cellImage.attr("src", imageList[event.key]);
         cellImage.fadeIn();
       }
+    } else {
+      return;
     }
-  })
 
-  //trees
-  $(document).keydown(function(event) {
-      let image, dx, dy, scale, data;
-
-      if (event.key === "q") {
-        image = $('<img>', {
-          src: trees.oak.location
-        });
-        dx = -trees.oak.ox;
-        dy = -trees.oak.oy;
-        scale = trees.oak.scale;
-        data = trees.oak.data;
-      } else if (event.key === "w") {
-        image = $('<img>', {
-          src: trees.birch.location
-        });
-        dx = -trees.birch.ox;
-        dy = -trees.birch.oy;
-        scale = trees.birch.scale;
-        data = trees.birch.data;
-      } else if (event.key === "e") {
-        image = $('<img>', {
-          src: trees.pine.location
-        });
-        dx = -trees.pine.ox;
-        dy = -trees.pine.oy;
-        scale = trees.pine.scale;
-        data = trees.pine.data;
-      } else {
-        return;
-      }
-
-      //add the tree imahge
+    //add the tree imahge
+    if (image) {
       image.css({
         position: 'absolute',
         left: mouseX+dx+'px',
@@ -166,5 +238,8 @@ $(document).ready(function() {
             }
         }
       }
+    }
+
+    updateSoil();
   });
 });
